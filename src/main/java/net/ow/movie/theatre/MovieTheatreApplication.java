0b22522febc3183c7f60e426d9c92391c6cb459a
Oliver <hohang210@gmail.com>
2024-12-26 22:30:42 +0800

    [FEAT] Update main class to run as spring boot application

--- src/main/java/net/ow/movie/theatre/MovieTheatreApplication.java
+++ src/main/java/net/ow/movie/theatre/MovieTheatreApplication.java
@@ -1,5 +1,11 @@
 package net.ow.movie.theatre;
 
-public class MovieTheatreApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class MovieTheatreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTheatreApplication.class, args);
+    }
 }
